b274988669e34ccbaeb679af3209deb1a1c28713
Ambreen <106621236+AmbreenAbro@users.noreply.github.com>
2024-06-27 15:11:11 +0530

    Add files via upload

---
+++ myarr.js
@@ -0,0 +1,31 @@
+var myArr = ["Fan","Abro",67,89,null];
+//myArr.pop();
+//myArr.push("Ambreen");
+//myArr.shift();
+//myArr.unshift();
+//myArr.
+console.log(myArr.length);
+// String method in js
+mystr="My name is Ambreen ";
+console.log(mystr.length);
+console.log(mystr.indexof("My"));
+console.log(myArr.slice(0,3));
+myDate=new Date();
+console.log(myDate.getTime());
+console.log(myDate.getFulYear());
+console.log(myDate.getDay());
+console.log(myDate.getHours());
+console.log(myDate.geMinutes());
+//DOM document object method
+let elem = document.getElementById('click')
+console.log(elem);
+// Events in JS
+function clicked(){
+console.log('The button was clicked');    
+}
+window.onload=function(){
+console.log('The document was loaded');    
+
+}
+//Arrow function
+
